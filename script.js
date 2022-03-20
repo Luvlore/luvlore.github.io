14b3ea12baf1155cd86efbbfbb7956e5560c3e85
Bruna Luchini <brunaluchini22@gmail.com>
2022-03-20 18:30:11 -0300

    alteração no script e style

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ subtitle.style.left = 0;
 
 window.onscroll = () => {
   let value = window.scrollY;
-  moon.style.top = value * 1.15 + 'px';
+  moon.style.top = value * 1.2 + 'px';
   mountainBehind.style.top = value * 1 +'px';
   mountainMiddle.style.top = value * 0.6 +'px';
-  mountainFront.style.top = value * 0.3 +'px';
+  mountainFront.style.top = value * 0.2 +'px';
   
   title.style.marginTop = value * 0.7 + 'px';
   subtitle.style.left = value * 2 + 'px';
   subtitle.style.marginTop = value * 0.7 + 'px';
-  subtitle.style.marginLeft = value * 3 + 'px';
+  subtitle.style.marginLeft = value * 1.7 + 'px';
   // subtitle.style.bottom = value * 9 + 'px';
 }
